e599ba5508cd122b22cca94c29bba9e9c8a14cce
yaosuhuy <levanquanghuy8@gmail.com>
2025-02-20 00:06:33 +0700

    feed UI completed

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/ultis/colors.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: mobileBackgroundColor,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 16,
+            ).copyWith(right: 0),
+            child: Row(
+              children: [
+                // HEADER SECTION
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage(
+                      'https://cdn3.iconfinder.com/data/icons/avatars-add-on-pack-2/48/v-32-512.png'),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 8,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'username',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                              child: ListView(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 16,
+                                ),
+                                shrinkWrap: true,
+                                children: [
+                                  'Delete',
+                                ]
+                                    .map(
+                                      (e) => InkWell(
+                                        onTap: () {},
+                                        child: Container(
+                                          padding: const EdgeInsets.symmetric(
+                                            vertical: 12,
+                                            horizontal: 16,
+                                          ),
+                                          child: Text(e),
+                                        ),
+                                      ),
+                                    )
+                                    .toList(),
+                              ),
+                            ));
+                  },
+                  icon: const Icon(Icons.more_vert),
+                )
+              ],
+            ),
+          ),
+          // IMAGE SECTION
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              'https://live.staticflickr.com/65535/49675866667_9ddefb5fd8_b.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          // LIKE, COMMENT SECTION
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.comment_outlined,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.send,
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.bookmark_border),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // DESCRIPTION AND NUMBER OF COMMENTS
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '1,231 likes',
+                  style: TextStyle(fontWeight: FontWeight.w800),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(
+                    top: 8,
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'username',
+                          style: const TextStyle(
+                            color: primaryColor,
+                          ),
+                        ),
+                        TextSpan(
+                          text: ' Hey this is the description',
+                          style: const TextStyle(
+                            color: primaryColor,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      'View all 200 comments',
+                      style: TextStyle(fontSize: 16, color: secondaryColor),
+                    ),
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      '22/12/2024',
+                      style: TextStyle(fontSize: 16, color: secondaryColor),
+                    ),
+                  ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
